412506a9e0bb3e7e308abff14ec5c8c6df2767e0
Mitchell Finlay <mitchell.finlay00@gmail.com>
2023-03-29 21:38:29 +1100

    done with assignment

--- index.js
+++ index.js
@@ -11,6 +11,15 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+
+function titleCased() {
+  const titleCased = tutorials.map((tutorial) => {
+    const words = tutorial.split(" ");
+    const titleCasedWords = words.map((word) => {
+      return word.charAt(0).toUpperCase() + word.slice(1);
+    });
+    
+    return titleCasedWords.join(" ");
+  });
+  return titleCased
 }
